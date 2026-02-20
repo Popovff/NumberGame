8f05d772c426b0333987a5c8508082e14fd66a22
Popovff <rentik2006@gmail.com>
2026-02-20 16:41:26 +0900

    Initial commit: игра Угадай число

---
+++ game.py
@@ -0,0 +1,27 @@
+import random
+
+def check_guess(secret, guess):
+    """Сравнивает догадку с загаданным числом и возвращает подсказку."""
+    if guess < secret:
+        return "Слишком мало"
+    elif guess > secret:
+        return "Слишком много"
+    else:
+        return "Поздравляю! Вы угадали!"
+
+def main():
+    secret = random.randint(1, 100)
+    print("Я загадал число от 1 до 100. Попробуйте угадать!")
+    while True:
+        try:
+            guess = int(input("Ваше предположение: "))
+        except ValueError:
+            print("Пожалуйста, введите число.")
+            continue
+        result = check_guess(secret, guess)
+        print(result)
+        if result == "Поздравляю! Вы угадали!":
+            break
+
+if __name__ == "__main__":
+    main()
